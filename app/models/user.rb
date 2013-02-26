9a7e4129c688fe69503c63e2e076c38a0e4f3e95
Andrew Brown <ajbpn7@mst.edu>
2013-02-26 02:07:37 -0600

    see content of users with model functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,29 @@ class User
 
   validates_presence_of :name, :email, :password
   validates_uniqueness_of :name, :email
+
+  def characters
+    Character.where(user_id: id)
+  end
+
+  def equipment
+    Equipment.where(user_id: id)
+  end
+
+  def languages
+    Language.where(user_id: id)
+  end
+
+  def locations
+    Location.where(user_id: id)
+  end
+
+  def magics
+    Magic.where(user_id: id)
+  end
+
+  def universes
+    Universe.where(user_id: id)
+  end
+  
 end
