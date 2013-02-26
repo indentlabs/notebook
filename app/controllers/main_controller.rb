ffb167f0f580f0e2ab5c63c1cc06d206f3bff051
Andrew Brown <ajbpn7@mst.edu>
2013-02-25 21:27:19 -0600

    initial nonlinking stats on dashboard

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
+  before_filter :redirect_if_not_logged_in, :only => [:dashboard]
+  
   def index
   end
   
@@ -6,5 +8,14 @@ class MainController < ApplicationController
   end
   
   def dashboard
+    @characters = Character.where(user_id: session[:user])
+    @equipment = Equipment.where(user_id: session[:user])
+    @languages = Language.where(user_id: session[:user])
+    @locations = Location.where(user_id: session[:user])
+    @magics = Magic.where(user_id: session[:user])
+    @universes = Universe.where(user_id: session[:user])
+    
+    @things = [ @characters.length, @equipment.length, @languages.length, 
+                @locations.length, @magics.length, @universes.length ].sum
   end
 end
