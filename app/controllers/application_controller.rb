ce79faa6603e8112d685d70516e356f63f0234e9
Andrew Brown <drusepth@gmail.com>
2013-03-10 20:32:32 -0500

    escape nasty html and give index columns widths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
   
   # View Helpers
   def nl2br(string)
+    #simple_format string
     string.gsub("\n\r","<br>").gsub("\r", "").gsub("\n", "<br />").html_safe
   end
   
