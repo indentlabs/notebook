0bf96669376b95e58a5fbc9f0523f7bc8219ebb4
Andrew Brown <drusepth@gmail.com>
2013-12-08 19:39:39 -0600

    garbage change to push to heroku again to trigger push hooks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ PlanCharacters::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -62,3 +62,4 @@ PlanCharacters::Application.configure do
   config.active_support.deprecation = :notify
 
 end
+
