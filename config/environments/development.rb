666e7ac260186c72dc0e9a3fbb8031ffe4899e81
Andrew Brown <drusepth@gmail.com>
2014-01-07 19:18:55 +0000

    add dev s3 bucket for testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,18 @@ PlanCharacters::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-
   # Do not compress assets
   config.assets.compress = false
+
+  # DEVELOPMENT S3 settings for Paperclip uploads ON DEVELOPMENT
+  config.paperclip_defaults = {
+    :storage        => :s3,
+    :s3_protocol    => 'http',
+    :s3_credentials => {
+      :bucket            => ENV['AWS_BUCKET'],
+      :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
