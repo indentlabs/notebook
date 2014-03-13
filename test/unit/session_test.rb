ff5a24dba86991bf9a6f2916f381ac023586c9e6
Robert Richter <robert.c.richter@gmail.com>
2014-03-12 23:42:18 -0500

    Fix undefined method errors in test cases

--- test/unit/session_test.rb
+++ test/unit/session_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class SessionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "session exists" do
+    assert_not_nil sessions(:one), "Sessions test fixture is inaccessible"
+    assert_not_nil sessions(:two), "Sessions test fixture is inaccessible"
+  end
 end
