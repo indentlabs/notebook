f3f0e829eafdb7b5b4630c93c030ce69d01900d8
Robert Richter <robert.c.richter@gmail.com>
2014-03-16 22:24:15 -0500

    Factories create sequence of usernames and emails

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -1,7 +1,13 @@
-FactoryGirl.define do
-  factory :user do
-    name "MyString"
-    email "MyString"
-    password "MyString"
-  end
+FactoryGirl.sequence :email do |n|
+  "test#{n}@example.com"
+end
+
+FactoryGirl.sequence :username do |n|
+  "testuser#{n}"
+end
+
+FactoryGirl.define :user do |user|
+  user.name { FactoryGirl.next(:username) }
+  user.email { FactoryGirl.next(:email) }
+  user.password "MyString"
 end
