7ba5abdf0aedf229598932895d3a05ae8bc27213
Robert Richter <robert.c.richter@gmail.com>
2014-03-16 19:03:17 -0500

    Attempt to get tests to work with Mongoid

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,5 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
   fixtures :all
 end
