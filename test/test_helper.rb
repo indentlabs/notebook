559822f63a0985694c3b4653944f63f6b22a488f
Robert Richter <robert.c.richter@gmail.com>
2014-03-16 21:21:26 -0500

    Get factory_girl working *correctly*

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'factory_girl_rails'
 
-FactoryGirl.find_definition
+FactoryGirl.find_definitions
 
 class ActiveSupport::TestCase
   include FactoryGirl::Syntax::Methods
