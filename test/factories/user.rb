d381c0978fcb79230449e7529c734ff650cf4ef4
Robert Richter <robert.c.richter@gmail.com>
2014-03-16 22:39:27 -0500

    Do sequences correctly

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -1,13 +1,15 @@
-FactoryGirl.sequence :email do |n|
-  "test#{n}@example.com"
-end
+FactoryGirl.define do
+  sequence :name do |n|
+    "testuser#{n}"
+  end
+  
+  sequence :email do |n|
+    "test#{n}@example.com"
+  end
 
-FactoryGirl.sequence :username do |n|
-  "testuser#{n}"
-end
-
-FactoryGirl.define :user do |user|
-  user.name { FactoryGirl.next(:username) }
-  user.email { FactoryGirl.next(:email) }
-  user.password "MyString"
+  factory :user do
+    name
+    email
+    password "MyString"
+  end
 end
