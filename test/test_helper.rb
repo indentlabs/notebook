c37c2502b7f1f4925ad92bbfd2d82abd804f0697
Robert Richter <robert.c.richter@gmail.com>
2014-03-16 21:31:14 -0500

    Iron out more factory_girl usage mistakes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,6 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'factory_girl_rails'
-
-FactoryGirl.find_definitions
 
 class ActiveSupport::TestCase
   include FactoryGirl::Syntax::Methods
