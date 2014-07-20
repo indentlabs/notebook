8adc24e9e5c9fe4a609d7eb5b95d889036a800e7
Andrew Brown <drusepth@gmail.com>
2014-07-20 03:19:41 -0500

    character page form

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,6 +1,6 @@
 module FormHelper
   def generate_form_row_for(form_handler, field, label_override = nil, toolbox = {})
-    label = label_override.nil? ? field : label_override
+    label = (label_override.nil? ? field : label_override.titleize)
     [
       '<div class="row">',
         '<div class="col-xs-2" style="text-align: right;">',
@@ -20,42 +20,4 @@ module FormHelper
     ].join("\n").html_safe
   end
 
-
-  def generate_picker_code_for(content_array, glyphicon_id)
-    return if content_array.length == 0
-    [
-      '<div class="input-group-btn">',
-        '<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">',
-          '<span class="glyphicon glyphicon-'+glyphicon_id+'"></span>',
-          '<span class="caret"></span>',
-        '</button>',
-        '<ul class="dropdown-menu">',
-          content_array.map { |content|
-            '<li><a href="#">' + content.name + '</a></li>'
-          },
-        '</ul>',
-      '</div>'
-    ].join("\n").html_safe
-  end
-
-  def character_picker
-    generate_picker_code_for(my_characters, 'user')
-  end
-  
-  def universe_picker
-    generate_picker_code_for(my_universes, 'globe')
-  end
-  
-  def equipment_picker
-    generate_picker_code_for(my_equipment, 'gift')
-  end
-  
-  def language_picker
-    generate_picker_code_for(my_languages, 'comment')
-  end
-  
-  def location_picker
-    generate_picker_code_for(my_locations, 'road')
-  end
-  
 end
