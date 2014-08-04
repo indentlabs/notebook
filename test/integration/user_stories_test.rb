6e30c97a6b3a3779090f9dce40ec0214f2972a8a
Robert Richter <robert.c.richter@gmail.com>
2014-08-03 22:28:37 -0500

    Add more integration tests and refactor

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -1,14 +1,33 @@
 require 'test_helper'
 
 class UserStoriesTest < ActionDispatch::IntegrationTest
-  test 'creating a new user' do
-    visit homepage_path
-    click_on 'Register'
-    fill_in 'Name', :with => 'tester'
-    fill_in 'Email', :with => 'test@example.com'
-    fill_in 'Password', :with => 'password'
-    click_on 'Create User'
+  fixtures :users
+  
+  test 'creating a new user ends at the new user\'s dashboard' do
+    register_as 'tester', 'test@example.com', 'password'
     
-    assert_equal dashboard_path, current_path, 'New user was not redirected to their dashboard after creating an account'
+    assert_equal dashboard_path, current_path, 'New user was not directed to their dashboard after creating an account'
+  end
+  
+  test 'logging in as an existing user goes to the users dashboard' do
+    log_in_as 'TestUser', 'TestPassword'
+    
+    assert_equal dashboard_path, current_path, 'Existing user was not directed to their dashboard after logging in'
+  end
+  
+  test 'logging in anonymousely goes into an empty dashboard' do
+    log_in_as_anon
+    
+    assert_equal dashboard_path, current_path, 'Anonymous user was not directed to their dashboard after logging in'
+  end
+  
+  ##
+  # Regression test for bug #366
+  
+  test 'Anonymous flag is reset on user logins' do
+    log_in_as_anon
+    log_out
+    log_in_as 'TestUser', 'TestPassword'
+    refute page.has_content?('You are currently using an anonymous account'), 'Logged-in user was told they were using an anonymous account, regression of #366'
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,4 +14,39 @@ end
 class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
+  
+  def register_as(name, email, password)
+    visit homepage_path
+    click_on 'Register'
+    fill_in 'user_name', :with => name
+    fill_in 'user_email', :with => email
+    fill_in 'user_password', :with => password
+    click_on 'Create User'
+  end
+  
+  def log_in_as(user, password)
+    visit homepage_path
+    click_on 'Login'
+    within('#new_session') do
+      fill_in 'session[username]', :with => user
+      fill_in 'session[password]', :with => password
+      click_on 'Log in'
+    end
+  end
+  
+  def log_in_as_anon
+    visit homepage_path
+    click_on 'Login'
+    click_on 'Be Anonymous'
+    click_on 'I understand, create an account for me'
+  end
+  
+  def log_out
+    visit logout_path
+  end
+  
+  def teardown
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
 end
