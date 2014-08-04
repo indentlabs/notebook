12ea8f8b4887b6bacee69e399d87a160b1d6d968
Robert Richter <robert.c.richter@gmail.com>
2014-08-03 22:27:04 -0500

    Include fix for #378

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,19 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :password, :email
   
+  before_save :hash_password
+
   has_many :characters
   has_many :equipment
   has_many :languages
   has_many :locations
   has_many :magics
   has_many :universes
+  
+  def hash_password
+    require 'digest'
+    self.password = Digest::MD5.hexdigest(self.name + "'s password IS... " + self.password + " (lol!)")
+  end
   
   def content
     {
