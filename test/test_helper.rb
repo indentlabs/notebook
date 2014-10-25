4945d1ca96daf30f7c3b22d13511ac10ad729589
Robert Richter <robert.c.richter@gmail.com>
2014-10-24 21:50:22 -0500

    Add Coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
+require 'coveralls'
+Coveralls.wear!('rails')
 
 class ActiveSupport::TestCase
   fixtures :all
