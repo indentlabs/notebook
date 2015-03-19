5a209d74ad0676e15feaeb5ca7444af5537d7ee9
Robert Richter <robert.c.richter@gmail.com>
2015-03-19 18:54:24 -0500

    Get coveralls up and running

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
 require 'coveralls'
-Coveralls.wear_merged!('rails')
+Coveralls.wear!('rails')
 
 class ActiveSupport::TestCase
   fixtures :all
