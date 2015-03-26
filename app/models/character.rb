dfd80d9539ed3a3f86037b4b65106f2f0dfadba7
Robert Richter <robert.c.richter@gmail.com>
2015-03-25 22:38:18 -0500

    Refactor CharactersController for style

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,16 @@
 class Character < ActiveRecord::Base
+  include Comparable
+  
   validates_presence_of :name
   
   belongs_to :user
   belongs_to :universe
+  
+  def <=>(anOther)
+    name.downcase <=> anOther.name.downcase
+  end
+  
+  def nil_blank_universe
+    self.universe = nil if self.universe.blank?
+  end
 end
