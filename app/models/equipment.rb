a6728af39784728d0487fe2e02723c365c8e2c53
Robert Richter <robert.c.richter@gmail.com>
2015-03-27 18:20:29 -0500

    Clean style in the EquipmentController

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,6 +1,18 @@
 class Equipment < ActiveRecord::Base
+  include Comparable
+  
   validates_presence_of :name
 
   belongs_to :user
   belongs_to :universe
+  
+  before_save :nil_blank_universe
+  
+  def <=>(anOther)
+    name.downcase <=> anOther.name.downcase
+  end
+
+  def nil_blank_universe
+    self.universe = nil if universe.blank?
+  end
 end
