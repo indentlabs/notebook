695cdc4f05bf8e2619a5b753e6e1b1aaaa89d359
Robert Richter <robert.c.richter@gmail.com>
2015-04-19 13:33:19 -0500

    Migrate to built-in ActiveModel secure passwords

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,6 +75,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :email)
+    params.require(:user).permit(:name, :email,
+                                 :password, :password_confirmation)
   end
 end
