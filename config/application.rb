6e38ab5c39d5797dc8822684751a2732ad6d59be
Robert Richter <robert.c.richter@gmail.com>
2015-04-23 21:56:44 -0500

    Allow l10n files to exist in a folder structure

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,10 @@ module PlanCharacters
     # The default locale is :en and all translations
     # from config/locales/*.rb,yml are auto loaded.
     #
-    # config.i18n.load_path += Dir[Rails.root.join(
-    #                              'my', 'locales', '*.{rb,yml}').to_s]
+
     # config.i18n.default_locale = :de
+    config.i18n.load_path +=
+      Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
     # Filter sensitive parameters out of logs
     config.filter_parameters << :password
