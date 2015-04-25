0a039c43156de223f73b75e7f1bbfd4237b3e41b
Robert Richter <robert.c.richter@gmail.com>
2015-04-25 13:12:23 -0500

    Add locale validation

--- app/controllers/concerns/localized.rb
+++ app/controllers/concerns/localized.rb
@@ -17,15 +17,34 @@ module Localized
   # 2. the +HTTP_ACCEPT_LANGUAGE+ header property
   # 3. the default locale in +I18n.default_locale+
   def set_locale
-    I18n.locale = params[:locale] ||
-      locale_from_accept_language_header ||
-      I18n.default_locale
+    I18n.locale = requested_locale || I18n.default_locale
+  end
+
+  # The locale requested by the user. Returns the first available value:
+  # 1. a URL paramter called "locale"
+  # 2. the +HTTP_ACCEPT_LANGUAGE+ header property
+  def requested_locale
+      validate_locale locale_from_url_params ||
+                        locale_from_accept_language_header
+  end
+
+  # Returns the given locale if localizations for it are available.
+  # Returns nil otherwise.
+  def validate_locale(locale)
+    return if locale.blank?
+    locale if locale.to_sym.in?(I18n.available_locales)
+  end
+
+  # The locale in the URL paramters
+  def locale_from_url_params
+    params[:locale]
   end
 
   # The two-character locale in the +HTTP_ACCEPT_LANGUAGE+ header field
   def locale_from_accept_language_header
-    return unless request
-    return unless request.env['HTTP_ACCEPT_LANGUAGE']
+    return if request.blank?
+    return if request.env['HTTP_ACCEPT_LANGUAGE'].blank?
+
     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 end
