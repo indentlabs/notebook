c0df16d7e0a241107bed194fab5ee7f1df9465a9
Andrew Brown <andrew@gomoment.com>
2016-05-03 12:57:00 -0500

    Add content groupers / organize models

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,7 @@ Bundler.require(*Rails.groups)
 module PlanCharacters
   # Default application environment configurations
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those
-    # specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{*/}')]
 
     # Set Time.zone default to the specified zone and make Active Record auto-
     # convert to this zone. Run "rake -D time" for a list of tasks for finding
@@ -23,8 +20,7 @@ module PlanCharacters
     # The default locale is :en and all translations
     # from config/locales/*.rb,yml are auto loaded.
     #
-    # config.i18n.load_path += Dir[Rails.root.join(
-    #                              'my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
     # Filter sensitive parameters out of logs
