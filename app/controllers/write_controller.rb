dbe6118efe3b99355242f8150a62f1e6a8be8fb7
Andrew Brown <andrew@gomoment.com>
2016-07-31 01:36:54 -0500

    Lean entity extraction + contextual card popups in editor

--- app/controllers/write_controller.rb
+++ app/controllers/write_controller.rb
@@ -2,5 +2,9 @@ class WriteController < ApplicationController
   layout 'editor'
 
   def editor
+    # TODO: Allow writers to specify a universe to scope content to, instead of all content
+    @characters = current_user.characters
+    @locations  = current_user.locations
+    @items      = current_user.items
   end
 end
