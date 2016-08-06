e571b2e50fb4daa6d917196bd6f317db7bae1966
Andrew Brown <andrew@gomoment.com>
2016-08-06 02:15:54 -0500

    Don't throw a 500 if we can't generate a question about content, just don't ask the question

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -21,7 +21,10 @@ class ContentController < ApplicationController
     # TODO: Secure this with content class whitelist lel
     @content = content_type_from_controller(self.class).find(params[:id])
     #question = QuestionService.question(Content.new @content.slice(*content_param_list.flat_map { |v| v.is_a?(Symbol) ? v : v.keys.map { |k| k.to_s.chomp('_attributes').to_sym } }))
-    @question = QuestionService.question(Content.new @content.slice(*content_param_list.reject { |x| x.is_a?(Hash) }))
+    begin
+      @question = QuestionService.question(Content.new @content.slice(*content_param_list.reject { |x| x.is_a?(Hash) }))
+    rescue
+    end
 
     respond_to do |format|
       format.html # show.html.erb
