eda53fc1e229aff68404288cbda13333ab0907d6
Robert Richter <robert.c.richter@gmail.com>
2016-08-26 14:50:57 -0500

    Remove useless tests

--- test/controllers/siblingship_controller_test.rb
+++ test/controllers/siblingship_controller_test.rb
@@ -1,14 +1,5 @@
 require 'test_helper'
 
 class SiblingshipControllerTest < ActionController::TestCase
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
-  end
-
+  # Nothing to test in the SiblingshipController yet
 end
