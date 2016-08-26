1b683ee2a00be568deed72deb44c965bc2580cda
Robert Richter <robert.c.richter@gmail.com>
2016-08-26 14:41:05 -0500

    Add setup to editor controller test

--- test/controllers/write_controller_test.rb
+++ test/controllers/write_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class WriteControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+  setup do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @user = create(:user)
+
+    sign_in @user
+  end
+
   test "should get editor" do
-    get :editor
+    get :editor, scene_id: 1
     assert_response :success
   end
 
