a0a0f787bb43e2bae8d288dd56f1843f91a82337
Andrew Brown <andrew@gomoment.com>
2016-09-01 23:37:34 -0500

    Send emails from notebook@notebook.ai instead

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'andrew@notebook.ai'
+  config.mailer_sender = 'notebook@notebook.ai'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
