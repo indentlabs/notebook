ef91154641e5a792d0c5332cb6d6b36172add7de
Andrew Brown <andrew@gomoment.com>
2016-09-06 00:15:07 -0500

    Add test for hitting author profile page successfully

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
+  test "should get user profile without error" do
+    test_user = User.create(email: 'test+test@test.test', password: 'testtesttest')
+
+    get :show, id: test_user.id
     assert_response :success
   end
 
