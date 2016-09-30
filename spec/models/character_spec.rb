e39c9fb463f0d5ed9dde8409d1af43651a1223db
Robert Richter <robert.c.richter@gmail.com>
2016-09-30 18:57:43 -0500

    Migrate Character tests to rspec

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Character, type: :model do
-  #it_behaves_like 'content with privacy'
+  context 'when name is nil' do
+    subject { build(:character, name: nil) }
+    it { is_expected.to_not be_valid }
+  end
+
+  context 'when character is in a public universe' do
+    let(:universe) { create(:universe, privacy: 'public') }
+
+    context 'when character is private' do
+      let(:character) { create(:character, universe: universe, privacy: 'private') }
+
+      describe 'Character.is_public' do
+        subject { Character.is_public }
+        it { is_expected.to include(character) }
+      end
+    end
+
+    context 'when character is public' do
+      let(:character) { create(:character, universe: universe, privacy: 'public') }
+
+      describe 'Character.is_public' do
+        subject { Character.is_public }
+        it { is_expected.to include(character) }
+      end
+    end
+  end
+
+  context 'when character is in a private universe' do
+    let(:universe) { create(:universe, privacy: 'private') }
+
+    context 'when character is private' do
+      let(:character) { create(:character, universe: universe, privacy: 'private') }
+
+      describe 'Character.is_public' do
+        subject { Character.is_public }
+        it { is_expected.to_not include(character) }
+      end
+    end
+
+    context 'when character is public' do
+      let(:character) { create(:character, universe: universe, privacy: 'public') }
+
+      describe 'Character.is_public' do
+        subject { Character.is_public }
+        it { is_expected.to include(character) }
+      end
+    end
+  end
 end
