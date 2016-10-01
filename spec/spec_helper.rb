c7d21ef621bded10eeef2d3e5b10e4c9c5ed9bae
Robert Richter <robert.c.richter@gmail.com>
2016-09-30 20:17:02 -0500

    Clean database before each spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,4 +96,15 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
