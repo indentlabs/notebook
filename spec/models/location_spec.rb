4fb6e0ebf7aa0243688af5c6c531b50d3cdccb3a
Robert Richter <robert.c.richter@gmail.com>
2016-09-30 20:22:17 -0500

    Add location spec

---
+++ spec/models/location_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require 'support/privacy_example'
+require 'support/public_scope_example'
+require 'support/name_validation_example'
+
+RSpec.describe Location, type: :model do
+  it_behaves_like 'content with privacy'
+  it_behaves_like 'content with an is_public scope'
+  it_behaves_like 'content that validates presence of name'
+end
