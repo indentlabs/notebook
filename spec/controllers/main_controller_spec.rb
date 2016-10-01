3ad2a191727a9692fc76612ef3101fdcc0e02b14
Robert Richter <robert.c.richter@gmail.com>
2016-10-01 13:33:27 -0500

    Write main and write controller spec

---
+++ spec/controllers/main_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+require 'support/devise'
+
+RSpec.describe MainController, :type => :controller do
+  describe 'GET #index' do
+    before { get :index }
+    it { is_expected.to respond_with(200) }
+  end
+end
---
+++ spec/controllers/write_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+require 'support/devise'
+
+RSpec.describe WriteController, :type => :controller do
+  describe 'GET #editor, scene_id: 1' do
+    before do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      @user = create(:user)
+
+      sign_in @user
+
+      get :editor, scene_id: 1
+    end
+    
+    it { is_expected.to respond_with(200) }
+  end
+end
