f45e5d956df58476e8541cb77c5f701a390d14d4
Robert Richter <robert.c.richter@gmail.com>
2016-09-30 20:20:03 -0500

    Extract name validation spec

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,13 +1,10 @@
 require 'rails_helper'
 require 'support/privacy_example'
 require 'support/public_scope_example'
+require 'support/name_validation_example'
 
 RSpec.describe Character, type: :model do
   it_behaves_like 'content with privacy'
   it_behaves_like 'content with an is_public scope'
-
-  context 'when name is nil' do
-    subject { build(:character, name: nil) }
-    it { is_expected.to_not be_valid }
-  end
+  it_behaves_like 'content that validates presence of name'
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
+require 'support/privacy_example'
+require 'support/public_scope_example'
+require 'support/name_validation_example'
 
 RSpec.describe Item, type: :model do
   it_behaves_like 'content with privacy'
   it_behaves_like 'content with an is_public scope'
-
-  context 'when name is nil' do
-    subject { build(:item, name: nil) }
-    it { is_expected.to_not be_valid }
-  end
+  it_behaves_like 'content that validates presence of name'
 end
