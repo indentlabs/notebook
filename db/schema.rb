b0b57e2e40b106e91ab63735cbcfa0548b1cb47e
Andrew Brown <drusepth@gmail.com>
2016-10-05 12:42:51 +0200

    Don't prepopulate author name to 'Anon author' on signup

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20161003000856) do
+
   create_table "archenemyships", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "character_id"
