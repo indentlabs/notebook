ddc3f378d51076e3058ab52dce0bd5cb5ec5d153
Andrew Brown <drusepth@gmail.com>
2016-10-15 00:33:26 +0200

    Content type model organization

---
+++ app/models/content_types/race.rb
@@ -0,0 +1,60 @@
+##
+# = race
+# == /'reis/
+# _noun_
+#
+# 1. each of the major divisions of sentient life, having distinct physical characteristics
+class Race < ActiveRecord::Base
+  validates :name, presence: true
+
+  belongs_to :user
+  validates :user_id, presence: true
+
+  belongs_to :universe
+
+  include HasPrivacy
+  include HasContentGroupers
+  include Serendipitous::Concern
+
+  # Characters
+  # relates :famous_figures, with: :racial_figure #wow wording
+
+  scope :is_public, -> { eager_load(:universe).where('creatures.privacy = ? OR universes.privacy = ?', 'public', 'public') }
+
+  def self.color
+    'grey'
+  end
+
+  def self.icon
+    'face'
+  end
+
+  def self.attribute_categories
+    {
+      overview: {
+        icon: 'info',
+        attributes: %w(name description other_names universe_id)
+      },
+      looks: {
+        icon: 'face',
+        attributes: %w(body_shape skin_colors height weight notable_features variance clothing)
+      },
+      traits: {
+        icon: 'fingerprint',
+        attributes: %w(strengths weaknesses)
+      },
+      culture: {
+        icon: 'groups',
+        attributes: %w(traditions beliefs governments technologies occupations economics favorite_foods)
+      },
+      history: {
+        icon: 'info',
+        attributes: %w(notable_events)
+      },
+      notes: {
+        icon: 'edit',
+        attributes: %w(notes private_notes)
+      }
+    }
+  end
+end
