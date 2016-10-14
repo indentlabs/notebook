1f4198828af156ecdb5bedd244c6b47ac32219e3
Andrew Brown <drusepth@gmail.com>
2016-10-14 23:34:59 +0200

    Clean up redundant views

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,17 @@
+class CreaturesController < ContentController
+  private
+
+  def content_params
+    params.require(:creature).permit(content_param_list)
+  end
+
+  def content_param_list
+    %i(
+      name description type_of other_names universe_id color shape size notable_features
+      materials preferred_habitat sounds strengths weaknesses spoils aggressiveness
+      attack_method defense_method maximum_speed food_sources
+      migratory_patterns reproduction herd_patterns
+      similar_animals symbolisms
+    )
+  end
+end
---
+++ app/models/creature.rb
@@ -0,0 +1,62 @@
+##
+# = cree-chur
+# == /'krechurr/
+# _noun_
+#
+# 1. an animal, plant, or other wildlife occuring in a user's story
+#
+#    exists within a Universe.
+class Creature < ActiveRecord::Base
+  validates :name, presence: true
+
+  belongs_to :user
+  validates :user_id, presence: true
+
+  belongs_to :universe
+
+  include HasPrivacy
+  include HasContentGroupers
+  include Serendipitous::Concern
+
+  # Locations
+  #relates :found_in,    with: :wildlifeship
+
+  scope :is_public, -> { eager_load(:universe).where('creatures.privacy = ? OR universes.privacy = ?', 'public', 'public') }
+
+  def self.color
+    'brown'
+  end
+
+  def self.icon
+    'pets'
+  end
+
+  def self.attribute_categories
+    {
+      overview: {
+        icon: 'info',
+        attributes: %w(name description type_of other_names universe_id)
+      },
+      looks: {
+        icon: 'face',
+        attributes: %w(color shape size notable_features materials)
+      },
+      traits: {
+        icon: 'fingerprint',
+        attributes: %w(preferred_habitat sounds strengths weaknesses spoils)
+      },
+      habitat: {
+        icon: 'groups',
+        attributes: %w(aggressiveness attack_method defense_method maximum_speed food_sources migratory_patterns reproduction herd_patterns)
+      },
+      comparisons: {
+        icon: 'info',
+        attributes: %w(similar_animals symbolisms)
+      },
+      # notes: {
+      #   icon: 'edit',
+      #   attributes: %w(notes private_notes)
+      # }
+    }
+  end
+end
