7186f59665912de9982d60f4d4d1b47671bf5da3
Andrew Brown <drusepth@gmail.com>
2017-01-20 21:42:57 +0000

    Add stripe customer id field to user #migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102182259) do
+ActiveRecord::Schema.define(version: 20170120213941) do
 
   create_table "archenemyships", force: :cascade do |t|
     t.integer  "user_id"
@@ -624,20 +624,22 @@ ActiveRecord::Schema.define(version: 20161102182259) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "email",                               null: false
+    t.string   "email",                                 null: false
     t.string   "old_password"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "encrypted_password",       default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",            default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "plan_type"
+    t.integer  "selected_billing_plan_id"
+    t.string   "stripe_customer_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
