7de7d7508dfb97e5eb8860b5ab538e3789127e5a
Andrew Brown <drusepth@gmail.com>
2017-01-21 17:48:03 +0000

    Allow users with no subscriptions to create up to 5 universes

--- app/authorizers/universe_core_content_authorizer.rb
+++ app/authorizers/universe_core_content_authorizer.rb
@@ -1,6 +1,6 @@
 class UniverseCoreContentAuthorizer < CoreContentAuthorizer
   def self.creatable_by? user
-    user.universes.count < user.active_billing_plans.map(&:universe_limit).max
+    user.universes.count < (user.active_billing_plans.map(&:universe_limit).max || 5)
   end
 
   def readable_by? user
