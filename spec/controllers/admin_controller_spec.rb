0019e894db7a81a77dfd80c7f6c794966ae13a96
Andrew Brown <drusepth@gmail.com>
2017-02-03 14:59:37 +0000

    Tests for admin auth

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -2,28 +2,62 @@ require 'rails_helper'
 require 'support/devise'
 
 RSpec.describe AdminController, type: :controller do
-  describe 'GET #dashboard' do
-    before { get :dashboard }
-    it { is_expected.to respond_with(200) }
+  describe 'logged in user' do
+    before do
+      user = create(:user)
+      sign_in user
+    end
+
+    describe 'GET #dashboard' do
+      before { get :dashboard }
+      it { is_expected.to respond_with(200) }
+    end
+
+    describe 'GET #universes' do
+      before { get :universes }
+      it { is_expected.to respond_with(200) }
+    end
+
+    describe 'GET #characters' do
+      before { get :characters }
+      it { is_expected.to respond_with(200) }
+    end
+
+    describe 'GET #locations' do
+      before { get :locations }
+      it { is_expected.to respond_with(200) }
+    end
+
+    describe 'GET #items' do
+      before { get :items }
+      it { is_expected.to respond_with(200) }
+    end
   end
 
-  describe 'GET #universes' do
-    before { get :universes }
-    it { is_expected.to respond_with(200) }
-  end
+  describe 'logged out user' do
+    describe 'GET #dashboard' do
+      before { get :dashboard }
+      it { is_expected.to respond_with(302) }
+    end
 
-  describe 'GET #characters' do
-    before { get :characters }
-    it { is_expected.to respond_with(200) }
-  end
+    describe 'GET #universes' do
+      before { get :universes }
+      it { is_expected.to respond_with(302) }
+    end
 
-  describe 'GET #locations' do
-    before { get :locations }
-    it { is_expected.to respond_with(200) }
-  end
+    describe 'GET #characters' do
+      before { get :characters }
+      it { is_expected.to respond_with(302) }
+    end
 
-  describe 'GET #items' do
-    before { get :items }
-    it { is_expected.to respond_with(200) }
+    describe 'GET #locations' do
+      before { get :locations }
+      it { is_expected.to respond_with(302) }
+    end
+
+    describe 'GET #items' do
+      before { get :items }
+      it { is_expected.to respond_with(302) }
+    end
   end
 end
