3450df2a04bdd3ea9eac3417b37fdfaabcb12299
Andrew Brown <drusepth@gmail.com>
2017-02-14 18:04:18 +0000

    Typo in tab disabling

--- app/helpers/attributes_helper.rb
+++ app/helpers/attributes_helper.rb
@@ -9,6 +9,6 @@ module AttributesHelper
     end
 
     link = content_tag(:a, category.label, href: "##{category.name.gsub("'", '')}_panel")
-    content_tag(:li, link, class: "tab #{is_disabled}")
+    content_tag(:li, link, class: "tab #{'disabled' if is_disabled}")
   end
 end
