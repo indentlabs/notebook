6941562141cdf28a4a87086d6d3d89e6ab5b122d
Andrew Brown <drusepth@gmail.com>
2017-02-16 20:31:20 +0000

    Pretty code is happy code :)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,10 +36,10 @@ PlanCharacters::Application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
+      bucket:            ENV.fetch('S3_BUCKET_NAME',        'test'),
+      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID',     'test'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', 'test'),
+      s3_region:         ENV.fetch('AWS_REGION',            'test'),
     }
   }
 
