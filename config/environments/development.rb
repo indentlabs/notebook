48af714fcea7c919634d37dc1d0206833fc22aad
Andrew Brown <drusepth@gmail.com>
2017-02-16 20:26:18 +0000

    Paperclip config for S3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,11 +35,11 @@ PlanCharacters::Application.configure do
   # DEVELOPMENT S3 settings for Paperclip uploads ON DEVELOPMENT
   config.paperclip_defaults = {
     storage: :s3,
-    s3_protocol: 'http',
     s3_credentials: {
-      bucket: ENV['AWS_BUCKET'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
 
