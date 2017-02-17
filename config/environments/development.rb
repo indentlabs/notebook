d072a4c7307b0d7b5175eed8f96abd03119cdad4
Andrew Brown <drusepth@gmail.com>
2017-02-17 22:25:16 +0000

    Show uploaded images on Gallery tab

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,10 +36,10 @@ PlanCharacters::Application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket:            ENV.fetch('S3_BUCKET_NAME',        'test'),
+      bucket:            ENV.fetch('S3_BUCKET_NAME',        'notebook-content-uploads'),
+      s3_region:         ENV.fetch('AWS_REGION',            'us-east-1'),
       access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID',     'test'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', 'test'),
-      s3_region:         ENV.fetch('AWS_REGION',            'test'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', 'test')
     }
   }
 
