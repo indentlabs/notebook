565d5b9db8418a7513b3818efa9f1a7b22a2f205
Andrew Brown <drusepth@gmail.com>
2017-02-18 01:13:43 +0000

    Add 50MB upload bandwidth to all users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216211914) do
+ActiveRecord::Schema.define(version: 20170218010814) do
 
   create_table "archenemyships", force: :cascade do |t|
     t.integer  "user_id"
@@ -665,15 +665,15 @@ ActiveRecord::Schema.define(version: 20170216211914) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "email",                                   null: false
+    t.string   "email",                                    null: false
     t.string   "old_password"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password",       default: "",   null: false
+    t.string   "encrypted_password",       default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",            default: 0,    null: false
+    t.integer  "sign_in_count",            default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -682,6 +682,7 @@ ActiveRecord::Schema.define(version: 20170216211914) do
     t.string   "stripe_customer_id"
     t.boolean  "email_updates",            default: true
     t.integer  "selected_billing_plan_id"
+    t.integer  "upload_bandwidth_kb",      default: 50000
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
