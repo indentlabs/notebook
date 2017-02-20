95211d6e1f97d772d4235d225b830be9474afa33
Andrew Brown <drusepth@gmail.com>
2017-02-20 16:06:58 +0000

    Add production paperclip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,10 +81,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
+      bucket:            ENV.fetch('S3_BUCKET_NAME',        'notebook-content-uploads'),
+      s3_region:         ENV.fetch('AWS_REGION',            'us-east-1'),
+      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
     }
   }
 
