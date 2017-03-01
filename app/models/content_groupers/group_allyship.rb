9b02da6e45b8e1d32fac6608a1a20f4e22e9fc9e
Andrew Brown <drusepth@gmail.com>
2017-03-01 14:41:21 +0000

    Make Group allies, enemies, and rivals reciprocal links

--- app/models/content_groupers/group_allyship.rb
+++ app/models/content_groupers/group_allyship.rb
@@ -5,4 +5,16 @@ class GroupAllyship < ActiveRecord::Base
 
   belongs_to :group
   belongs_to :ally, class_name: 'Group'
+
+  after_create do
+    self.reciprocate relation: :group_allyships, parent_object_ref: :group, added_object_ref: :ally
+  end
+
+  after_destroy do
+    # This is a two-way relation, so we should also delete the reverse association
+    this_object  = Group.find_by(id: self.group_id)
+    other_object = Group.find_by(id: self.ally_id)
+
+    other_object.allies.delete this_object
+  end
 end
--- app/models/content_groupers/group_enemyship.rb
+++ app/models/content_groupers/group_enemyship.rb
@@ -5,4 +5,16 @@ class GroupEnemyship < ActiveRecord::Base
 
   belongs_to :group
   belongs_to :enemy, class_name: 'Group'
+
+  after_create do
+    self.reciprocate relation: :group_enemyships, parent_object_ref: :group, added_object_ref: :enemy
+  end
+
+  after_destroy do
+    # This is a two-way relation, so we should also delete the reverse association
+    this_object  = Group.find_by(id: self.group_id)
+    other_object = Group.find_by(id: self.enemy_id)
+
+    other_object.enemies.delete this_object
+  end
 end
