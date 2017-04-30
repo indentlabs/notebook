efe33b445d36aca07813aec9c7ae311601cbceb5
Andrew Brown <drusepth@gmail.com>
2017-05-01 00:36:59 +0100

    Dynamically store relations on content classes

--- app/models/concerns/has_content_groupers.rb
+++ app/models/concerns/has_content_groupers.rb
@@ -4,6 +4,8 @@ module HasContentGroupers
   extend ActiveSupport::Concern
 
   included do
+    @@relations = {}
+
     # Example:
     #   relates :siblings, with: :siblingships, where: { alive: true }, type: :two_way
     #   Defines :siblings and :siblingships relations, their inverses, and accepts_nested_attributes for siblingships
@@ -11,6 +13,13 @@ module HasContentGroupers
       singularized_relation = relation.to_s.singularize
       connecting_class      = with.to_s.singularize.camelize.constantize
       connecting_class_name = with
+
+      # Store all added relations on the class model, so we can dynamically fetch them all whenever needed.
+      # e.g. Character.class_variable_get(:@@relations) => [:mothers, :fathers, :siblings, ...]
+      @@relations[relation] = {              # @@relations[:siblings] = {
+        with:          with,                 #   with:          :siblingships,
+        related_class: connecting_class      #   related_class: Siblingship
+      }                                      # }
 
       # Siblingships
       has_many connecting_class_name
