227a56c555247856bd8d4328cfb8e186d23a7293
Andrew Brown <drusepth@gmail.com>
2017-05-23 19:59:01 +0000

    Rename app internally from PlanCharacters to Notebook

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-PlanCharacters::Application.configure do
+Notebook::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
