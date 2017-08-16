b132c6cc33e9eda2190d3ecf6f48b6602ffd45ee
Andrew Brown <drusepth@gmail.com>
2017-08-17 00:27:45 +0200

    authorizer updates

--- app/authorizers/content_authorizer.rb
+++ app/authorizers/content_authorizer.rb
@@ -2,20 +2,23 @@ class ContentAuthorizer < ApplicationAuthorizer
   def readable_by? user
     [
       resource.user_id == user.id,
-      resource.respond_to?(:privacy)  && resource.privacy == 'public',
-      resource.respond_to?(:universe) && resource.universe.present? && resource.universe.privacy == 'public',
-      resource.respond_to?(:universe) && resource.universe && user.contributable_universes.pluck(:id).include?(resource.universe.id)
+      resource.respond_to?(:privacy) && resource.privacy == 'public',
+      resource.universe.present? && resource.universe.privacy == 'public',
+      resource.universe.present? && user.contributable_universes.pluck(:id).include?(resource.universe.id)
     ].any?
   end
 
   def updatable_by? user
     [
-    	resource.user_id == user.id,
-      resource.respond_to?(:universe) && resource.universe && user.contributable_universes.pluck(:id).include?(resource.universe.id)
+      resource.universe && user.contributable_universes.pluck(:id).include?(resource.universe.id),
+      resource.universe.nil? && resource.user_id == user.id,
     ].any?
   end
 
   def deletable_by? user
-  	resource.user_id == user.id
+    [
+      resource.universe && resource.universe.user == user.id,
+      resource.universe.nil? && resource.user_id == user.id
+    ].any?
   end
 end
--- app/authorizers/extended_content_authorizer.rb
+++ app/authorizers/extended_content_authorizer.rb
@@ -1,5 +1,11 @@
 class ExtendedContentAuthorizer < ContentAuthorizer
   def self.creatable_by? user
-    user.active_billing_plans.any? { |plan| plan.allows_extended_content } || user.selected_billing_plan_id == 2
+    [
+      user.active_billing_plans.any? { |plan| plan.allows_extended_content },
+      user.selected_billing_plan_id == 2,
+      user.contributable_universes.any? do |universe|
+        universe.user.active_billing_plans.any? { |plan| plan.allows_extended_content } || universe.user.selected_billing_plan_id == 2
+      end
+    ].any?
   end
 end
