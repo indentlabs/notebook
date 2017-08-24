91b4bc84f592c28fb1022d0cdf0c3293a9c4f7b4
Ahmad Saleem <ahmadsaleem93@gmail.com>
2017-08-25 01:15:31 +0500

    Find ominauth user only based on user id for a service provider.

--- app/models/omniauth_user.rb
+++ app/models/omniauth_user.rb
@@ -4,7 +4,7 @@ class OmniauthUser < ActiveRecord::Base
   validates :user, presence: true
 
   def self.from_omniauth(auth, current_user)
-    omniauth_user = find_by(provider: auth.provider, uid: auth.uid, email: auth.info.email)
+    omniauth_user = find_by(provider: auth.provider, uid: auth.uid)
     return omniauth_user.user if omniauth_user.present?
 
     user = current_user || User.find_by(email: auth.info.email) || User.create(email: auth.info.email, password: Devise.friendly_token[0,20])
