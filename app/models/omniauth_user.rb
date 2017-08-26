9816cc73f2d844777790ad726475f814949ce4de
Ahmad Saleem <ahmadsaleem93@gmail.com>
2017-08-26 13:32:22 +0500

    Assign name from social site while creating a user.

--- app/models/omniauth_user.rb
+++ app/models/omniauth_user.rb
@@ -7,8 +7,11 @@ class OmniauthUser < ActiveRecord::Base
     omniauth_user = find_by(provider: auth.provider, uid: auth.uid)
     return omniauth_user.user if omniauth_user.present?
 
-    user = current_user || User.find_by(email: auth.info.email) || User.create(email: auth.info.email, password: Devise.friendly_token[0,20])
-    user.omniauth_users.create(provider: auth.provider, uid: auth.uid, email: auth.info.email)
+    user = current_user ||
+           User.find_by(email: auth.info.email) ||
+           User.create(email: auth.info.email, password: Devise.friendly_token[0,20], name: auth.info.name)
+    user.omniauth_users.build(provider: auth.provider, uid: auth.uid, email: auth.info.email)
+    user.save
     user
   end
 end
