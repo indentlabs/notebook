9ac65972567c4faaed72f86912387be728fa1203
Andrew Brown <drusepth@gmail.com>
2017-10-01 13:17:25 +0200

    Send invitation emails to new collaborators

--- app/services/contributor_service.rb
+++ app/services/contributor_service.rb
@@ -12,20 +12,28 @@ class ContributorService < Service
 
     # If the user doesn't already have a Notebook.ai account, send them an invite
     if related_user.nil?
-      send_invite_email_to(email)
+      send_invite_email_to(inviter: universe.user, email: email, universe: universe)
     end
 
     # If the user does have a Notebook.ai account, send them an email letting them know they got added as a contributor
     if related_user
-      send_contributor_notice_to(email)
+      send_contributor_notice_to(inviter: universe.user, email: email, universe: universe)
     end
   end
 
-  def self.send_invite_email_to email
-    puts "sending invite email"
+  def self.send_invite_email_to inviter:, email:, universe:
+    CollaborationMailer.contributor_invitation(
+      inviter: inviter,
+      invite_email: email,
+      universe: universe
+    )
   end
 
   def self.send_contributor_notice_to email
-    puts "sending contrib notice"
+    CollaborationMailer.contributor_invitation(
+      inviter: inviter,
+      invite_email: email,
+      universe: universe
+    )
   end
 end
