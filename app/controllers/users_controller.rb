d27244217acc49123e75d2cf38a38375392f36d3
Andrew Brown <drusepth@gmail.com>
2017-11-25 05:06:31 +0100

    increase recent activity count

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
     @content = @user.public_content.select { |type, list| list.any? }
     @tabs = @content.keys
-    @stream = ContentChangeEvent.where(user_id: @user.id).order('id desc').limit(50)
+    @stream = ContentChangeEvent.where(user_id: @user.id).order('id desc').limit(100)
 
     Mixpanel::Tracker.new(Rails.application.config.mixpanel_token).track(@user.id, 'viewed profile', {
       'sharing any content': @user.public_content_count != 0
