45e1f26514d76ae0a762fe5209828fcfe86e906b
Andrew Brown <drusepth@gmail.com>
2017-12-26 19:15:19 -0600

    Send forum thread updates via noreply@notebook.ai

--- config/initializers/thredded.rb
+++ config/initializers/thredded.rb
@@ -65,10 +65,10 @@ Thredded.messageboards_order = :last_post_at_desc
 
 # ==> Email Configuration
 # Email "From:" field will use the following
-# Thredded.email_from = 'no-reply@example.com'
+Thredded.email_from = 'noreply@notebook.ai'
 
 # Emails going out will prefix the "Subject:" with the following string
-# Thredded.email_outgoing_prefix = '[My Forum] '
+Thredded.email_outgoing_prefix = '[Notebook.ai] '
 #
 # The parent mailer for all Thredded mailers
 # Thredded.parent_mailer = 'ActionMailer::Base'
