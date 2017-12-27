a7ba11c540a38d9c78c4cf151f52ca3d1d690629
Andrew Brown <drusepth@gmail.com>
2017-12-27 15:31:12 -0600

    Revert "temporary commenting out of old migration for staging"
    This reverts commit 93f4f341ceb684827729c00660bf53835a1e156f.

--- db/migrate/20161016213335_create_languages.rb
+++ db/migrate/20161016213335_create_languages.rb
@@ -1,22 +1,22 @@
 class CreateLanguages < ActiveRecord::Migration
   def change
-    # create_table :languages do |t|
-    #   t.string :name
-    #   t.string :other_names
-    #   t.integer :universe_id
-    #   t.integer :user_id
-    #   t.string :history
-    #   t.string :typology
-    #   t.string :dialectical_information
-    #   t.string :register
-    #   t.string :phonology
-    #   t.string :grammar
-    #   t.string :numbers
-    #   t.string :quantifiers
-    #   t.string :notes
-    #   t.string :private_notes
-    #
-    #   t.timestamps null: false
-    # end
+    create_table :languages do |t|
+      t.string :name
+      t.string :other_names
+      t.integer :universe_id
+      t.integer :user_id
+      t.string :history
+      t.string :typology
+      t.string :dialectical_information
+      t.string :register
+      t.string :phonology
+      t.string :grammar
+      t.string :numbers
+      t.string :quantifiers
+      t.string :notes
+      t.string :private_notes
+
+      t.timestamps null: false
+    end
   end
 end
