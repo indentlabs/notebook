807f116420adcc8e22f8f081a89e3c509b7ab786
Andrew Brown <drusepth@gmail.com>
2017-12-27 15:31:20 -0600

    Revert "temporarily comment out migration for staging"
    This reverts commit f49d056fe57353e2ff469f74c3ac027e683e1c55.

--- db/migrate/20161016211328_create_magics.rb
+++ db/migrate/20161016211328_create_magics.rb
@@ -1,25 +1,25 @@
 class CreateMagics < ActiveRecord::Migration
   def change
-    # create_table :magics do |t|
-    #   t.string :name
-    #   t.string :description
-    #   t.string :type_of
-    #   t.integer :universe_id
-    #   t.integer :user_id
-    #   t.string :visuals
-    #   t.string :effects
-    #   t.string :positive_effects
-    #   t.string :negative_effects
-    #   t.string :neutral_effects
-    #   t.string :element
-    #   t.string :resource_costs
-    #   t.string :materials
-    #   t.string :skills_required
-    #   t.string :limitations
-    #   t.string :notes
-    #   t.string :private_notes
-    #
-    #   t.timestamps null: false
-    # end
+    create_table :magics do |t|
+      t.string :name
+      t.string :description
+      t.string :type_of
+      t.integer :universe_id
+      t.integer :user_id
+      t.string :visuals
+      t.string :effects
+      t.string :positive_effects
+      t.string :negative_effects
+      t.string :neutral_effects
+      t.string :element
+      t.string :resource_costs
+      t.string :materials
+      t.string :skills_required
+      t.string :limitations
+      t.string :notes
+      t.string :private_notes
+
+      t.timestamps null: false
+    end
   end
 end
