79b626d9a26750b0cca0779e363b886c696945bd
Tristan <tristan@indentlabs.com>
2018-07-04 20:14:51 -0500

    rake rails:update

--- config/application.rb
+++ config/application.rb
@@ -1,38 +1,26 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'csv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Notebook
-  # Default application environment configurations
   class Application < Rails::Application
-    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{*/}')]
-    config.autoload_paths += Dir[Rails.root.join('app', 'services', '{*/}')]
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-
-    # convert to this zone. Run "rake -D time" for a list of tasks for finding
-    # time zone names. Default is UTC.
-    #
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations
-    # from config/locales/*.rb,yml are auto loaded.
-    #
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Use delayed_job to process jobs
-    config.active_job.queue_adapter = :delayed_job
-
-    # Filter sensitive parameters out of logs
-    config.filter_parameters << :password
-    config.filter_parameters << :password_confirmation
-
-    # Don't encode ampersands into \u0026 when creating JSON
-    config.active_support.escape_html_entities_in_json = false
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,22 +7,23 @@ Rails.application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
-  # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_files = true
-  config.static_cache_control = 'public, max-age=3600'
+  # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  # Log error messages when you accidentally call methods on nil
-  config.whiny_nils = true
+  # Configure static file server for tests with Cache-Control for performance.
+  config.serve_static_files   = true
+  config.static_cache_control = 'public, max-age=3600'
 
-  # Show full error reports and disable caching
+  # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Raise exceptions instead of rendering exception templates
+  # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
-  # Disable request forgery protection in test environment
+  # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
   # Tell Action Mailer not to deliver emails to the real world.
@@ -30,16 +31,12 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Print deprecation notices to the stderr
-  config.active_support.deprecation = :stderr
-
+  # Randomize the order test cases are executed.
   config.active_support.test_order = :random
 
-  config.active_record.raise_in_transactional_callbacks = true
+  # Print deprecation notices to the stderr.
+  config.active_support.deprecation = :stderr
 
-  default_url_options[:host] = 'test.host'
-
-  # Set test-mode Stripe API key
-  Stripe.api_key = "We-don't-want-to-actually-hit-Stripe-from-tests"
-  config.stripe_publishable_key = 'pk_test_eXI4iyJ2gR9UOGJyJERvDlHF'
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 end
