67f24b792c772e7a49e43d52cd3e5ed2cd475f6a
Andrew Brown <drusepth@gmail.com>
2018-08-23 23:40:04 -0500

    add version to migration

--- db/migrate/20180715184447_create_religion_deities.rb
+++ db/migrate/20180715184447_create_religion_deities.rb
@@ -1,4 +1,4 @@
-class CreateReligionDeities < ActiveRecord::Migration
+class CreateReligionDeities < ActiveRecord::Migration[4.2]
   def change
     create_table :religion_deities do |t|
       t.references :user, index: true, foreign_key: true
