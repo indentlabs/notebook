6d0af8a50db46ec050ddf221ba14776e611306bb
Andrew Brown <drusepth@gmail.com>
2018-09-21 15:47:45 -0500

    fix indices

--- db/migrate/20180921182215_add_attribute_field_migrations.rb
+++ db/migrate/20180921182215_add_attribute_field_migrations.rb
@@ -1,8 +1,9 @@
 class AddAttributeFieldMigrations < ActiveRecord::Migration[5.2]
   def change
-    add_index :attribute_fields, [:user_id, :attribute_category_id, :label, :hidden, :deleted], name: 'field_lookup_by_label_index'
-    add_index :attribute_fields, [:user_id, :attribute_category_id, :field_type, :deleted], name: 'special_field_type_index'
+    add_index :attribute_fields, [:user_id, :attribute_category_id, :label, :hidden, :deleted_at], name: 'field_lookup_by_label_index'
+    add_index :attribute_fields, [:user_id, :attribute_category_id, :field_type, :deleted_at], name: 'special_field_type_index'
 
     add_index :attribute_fields, [:user_id, :attribute_category_id, :label, :old_column_source, :field_type], name: 'temporary_migration_lookup_index'
+    add_index :attribute_fields, [:user_id, :attribute_category_id, :label, :old_column_source, :field_type, :deleted_at], name: 'temporary_migration_lookup_with_deleted_index'
   end
 end
