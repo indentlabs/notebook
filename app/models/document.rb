18c40cecc71b324c47a483d2654e142f7739fa9b
Andrew Brown <drusepth@gmail.com>
2018-10-18 13:22:56 -0500

    add deleted_at usage to documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,8 @@
 class Document < ApplicationRecord
+  acts_as_paranoid
+
   belongs_to :user
+
+  include Authority::Abilities
+  self.authorizer_name = 'DocumentAuthorizer'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_224014) do
+ActiveRecord::Schema.define(version: 2018_10_18_182216) do
 
   create_table "api_keys", force: :cascade do |t|
     t.integer "user_id"
@@ -726,6 +726,7 @@ ActiveRecord::Schema.define(version: 2018_10_17_224014) do
     t.string "title", default: "Untitled document"
     t.string "privacy", default: "private"
     t.text "synopsis"
+    t.datetime "deleted_at"
     t.index ["user_id"], name: "index_documents_on_user_id"
   end
 
