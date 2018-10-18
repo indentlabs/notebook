17cbdda8319284dbd054690fb1d45d010137c07b
Andrew Brown <drusepth@gmail.com>
2018-10-18 13:43:02 -0500

    fill out controller actions

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,10 +1,22 @@
 class DocumentsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @documents = current_user.documents.order('title asc')
   end
 
+  def show
+    document = Document.find_by(id: params[:id])
+    redirect_to edit_document_path(document)
+  end
+
   def edit
     @document = Document.find_by(id: params[:id], user_id: current_user.id)
+  end
+
+  def create
+    created_document = current_user.documents.create(document_params)
+    redirect_to edit_document_path(created_document), notice: "Your document has been saved!"
   end
 
   def update
@@ -16,7 +28,7 @@ class DocumentsController < ApplicationController
     end
 
     document.update(document_params)
-    redirect_to(documents_path, notice: "Your document has been saved!")
+    redirect_to(edit_document_path(document), notice: "Your document has been saved!")
   end
 
   def destroy
@@ -29,6 +41,8 @@ class DocumentsController < ApplicationController
       redirect_back(fallback_location: root_path, notice: "You don't have permission to do that!")
     end
   end
+
+  private
 
   def document_params
     params.require(:document).permit(:title, :body)
