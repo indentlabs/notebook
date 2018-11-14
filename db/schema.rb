36498e045dc66051ad14c301b205482a9b96f0a5
Andrew Brown <drusepth@gmail.com>
2018-11-14 15:18:10 -0600

    docs landing page

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_11_01_234459) do
+
   create_table "api_keys", force: :cascade do |t|
     t.integer "user_id"
     t.string "key"
