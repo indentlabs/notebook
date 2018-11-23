c75977c754018d3a75477edc8134eee8d4ba3cdd
Andrew Brown <drusepth@gmail.com>
2018-11-23 09:14:33 -0600

    add ContentSerialier for caching attribute system per page load

---
+++ app/models/serializers/content_serializer.rb
@@ -0,0 +1,68 @@
+class ContentSerializer
+  attr_accessor :id, :name, :user
+
+  attr_accessor :categories
+  attr_accessor :fields
+  attr_accessor :attribute_values
+
+  attr_accessor :raw_model
+  attr_accessor :class_name, :class_color, :class_icon
+
+  attr_accessor :data
+  # name: 'blah,
+  # categories: [
+  #  {
+  #   name: 'category one',
+  #   fields: [{
+  #     id: field.name,
+  #     label: field.label,
+  #     value: field.attribute_values.find_by(..)
+  #   }]
+  # }
+
+  def initialize(content)
+    # One query per table; lets not muck with joins yet
+    self.attribute_values = Attribute.where(entity_type: content.page_type, entity_id: content.id)
+    self.fields           = AttributeField.where(id: self.attribute_values.pluck(:attribute_field_id))
+    self.categories       = content.class.attribute_categories(content.user)
+
+    self.id               = content.id
+    self.name             = content.name
+    self.user             = content.user
+
+    self.raw_model        = content
+
+    self.class_name       = content.class.name
+    self.class_color      = content.class.color
+    self.class_icon       = content.class.icon
+
+    self.data = {
+      name: content.try(:name),
+      description: content.try(:description),
+      universe: content.universe_id.nil? ? nil : {
+        id: content.universe_id,
+        name: content.universe.try(:name)
+      },
+      categories: self.categories.map { |category|
+        {
+          name:  category.name,
+          label: category.label,
+          icon:  category.icon,
+          fields: self.fields.select { |field| field.attribute_category_id == category.id }.map { |field|
+            {
+              id:    field.name,
+              label: field.label,
+              type:  field.field_type,
+              old_column_source: field.old_column_source,
+              value: self.attribute_values.detect { |value|
+                value.entity_type == content.page_type &&
+                value.entity_id   == content.id
+              }.try(:value) || ""
+            }
+          }
+        }
+      }
+    }
+  end
+
+end
