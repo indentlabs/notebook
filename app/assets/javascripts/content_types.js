42491107e6319541ea67973548ecb0649d807ddc
Andrew Brown <drusepth@gmail.com>
2019-01-05 16:26:24 -0600

    add these stupid badges because ie doesn't support filters

--- app/assets/javascripts/content_types.js
+++ app/assets/javascripts/content_types.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var content_type = $(this).data('content-type');
     var related_card = $(this).children('.card').first();
     var is_currently_active = related_card.hasClass('active');
+    var ie_badge = $(this).find('.enabled-badge');
 
     $.post('/customization/toggle_content_type', {
       content_type: content_type,
@@ -11,8 +12,10 @@ $(document).ready(function() {
 
     if (is_currently_active) {
       related_card.removeClass('active');
+      ie_badge.attr('data-badge-caption', 'hidden');
     } else {
       related_card.addClass('active');
+      ie_badge.attr('data-badge-caption', 'active');
     }
 
     // Return false so we don't jump to the top of the page on link click
