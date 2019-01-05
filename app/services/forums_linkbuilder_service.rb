6686fb0efbebfae58539edd4c4561891f52ddf7b
Andrew Brown <drusepth@gmail.com>
2019-01-05 17:10:57 -0600

    add new forum links

--- app/services/forums_linkbuilder_service.rb
+++ app/services/forums_linkbuilder_service.rb
@@ -6,15 +6,21 @@ class ForumsLinkbuilderService < Service
   def self.content_to_url_map
     {
       'Character': '/forum/characters-board',
+      'Condition': '/forum/conditions',
       'Creature': '/forum/characters', # [sic]
       'Flora': '/forum/flora',
       'Government': '/forum/governments',
+      'Item': '/forum/items',
+      'Job': '/forum/jobs',
+      'Landmark': '/forum/landmarks',
       'Language': '/forum/general-worldbuilding', # wtf did I do
       'Location': '/forum/locations',
       'Magic': '/forum/magic',
       'Planet': '/forum/planets',
+      'Race': '/forum/races',
       'Religion': '/forum/religions',
-      'Technology': '/forum/technology'
+      'Technology': '/forum/technology',
+      'Tradition': '/forum/traditions'
     }
   end
 end
