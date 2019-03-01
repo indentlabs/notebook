cc0b54308cc101a44c47ac838095a2e990fe2ec5
Andrew Brown <drusepth@gmail.com>
2019-03-01 16:37:47 -0600

    document category helper methods

--- app/models/document_category.rb
+++ app/models/document_category.rb
@@ -1,3 +1,11 @@
 class DocumentCategory < ApplicationRecord
   belongs_to :document_analysis
+
+  def parent_categories
+    self.label.split('/')[1..-2].join('/')
+  end
+
+  def terminal_category
+    self.label.split('/').last
+  end
 end
