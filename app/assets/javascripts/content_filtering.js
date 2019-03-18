482284a376e91035ab09455c52bf59cd50961664
Andrew Brown <drusepth@gmail.com>
2019-03-18 18:26:38 -0500

    case insensitivity on content filtering

--- app/assets/javascripts/content_filtering.js
+++ app/assets/javascripts/content_filtering.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+  // Override the :contains() selector to make it case-insensitive
+  $.expr[":"].contains = $.expr.createPseudo(function(arg) {
+    return function( elem ) {
+      return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+  });
+
   $('#js-content-name-filter').keyup(function (e) {
     var search_query = $(this).val();
     var content_list = $('.js-content-cards-list > .js-content-card-container');
