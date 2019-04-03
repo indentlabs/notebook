f29fa3271ad69c6976b9f77821ed815dd0acbdf3
Andrew Brown <drusepth@gmail.com>
2019-04-03 18:56:42 -0500

    only show new _content_ link in header if a user can follow it

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -523,7 +523,7 @@ class ContentController < ApplicationController
     @navbar_actions << {
       label: "New #{content_type.name.downcase}",
       href: main_app.new_polymorphic_path(content_type)
-    }
+    } if current_user.can_create?(content_type)
 
     discussions_link = ForumsLinkbuilderService.worldbuilding_url(content_type)
     if discussions_link.present?
