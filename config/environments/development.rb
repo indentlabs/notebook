d7bf8de903dc19d5b9dc373a5b97c4301039bee6
Andrew Brown <drusepth@gmail.com>
2019-06-01 00:56:52 -0500

    default values for aws in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,8 +51,8 @@ Rails.application.configure do
     s3_credentials: {
       bucket:            ENV.fetch('S3_BUCKET_NAME',        'notebook-content-uploads'),
       s3_region:         ENV.fetch('AWS_REGION',            'us-east-1'),
-      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID',     ''),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', '')
     }
   }
 
