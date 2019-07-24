0f7f9587e0a191bec6c472bbaed6646b846596b3
Andrew Brown <drusepth@gmail.com>
2019-07-23 19:58:12 -0500

    fix bug in document linking on content pages

--- app/models/concerns/is_content_page.rb
+++ app/models/concerns/is_content_page.rb
@@ -14,10 +14,10 @@ module IsContentPage
     has_many :document_entities, as: :entity
     attr_accessor :document_entity_id
     def documents
-      analysis_ids = DocumentAnalysis.where(
+      document_ids = DocumentAnalysis.where(
         id: document_entities.pluck(:document_analysis_id)
-      ).pluck(:id)
-      Document.where(id: analysis_ids)
+      ).pluck(:document_id)
+      Document.where(id: document_ids)
     end
   end
 end
