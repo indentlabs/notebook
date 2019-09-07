91ce96b8213a84969e3753b772d414ad1ab595af
Andrew Brown <drusepth@gmail.com>
2019-09-07 16:50:09 -0500

    confusing comment placement

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -76,17 +76,17 @@ class MainController < ApplicationController
 
   def set_random_content
     @activated_content_types.shuffle.each do |content_type|
-      if content_type.downcase == "universe"
+      if content_type == Universe.name
         if @universe_scope.present?
-          # when we want to enable prompts for contributing universes we can remove the user:
-          # selector here, but we will need to verify the user has permission to see the universe
-          # when we do that, or else prompts could open leak
           @content = content_type.constantize.where(user: current_user, id: @universe_scope.id).includes(:user)
         else
           @content = content_type.constantize.where(user: current_user).includes(:user)
         end
       else
         if @universe_scope.present?
+          # when we want to enable prompts for contributing universes we can remove the user:
+          # selector here, but we will need to verify the user has permission to see the universe
+          # when we do that, or else prompts could open leak
           @content = content_type.constantize.where(user: current_user, universe: @universe_scope).includes(:user)
         else
           @content = content_type.constantize.where(user: current_user).includes(:user)
