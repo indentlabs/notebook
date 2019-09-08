cdbf9130bad2ea635979f91c7d4652f4bb401b47
Andrew Brown <drusepth@gmail.com>
2019-09-07 21:20:39 -0500

    who let this past qa

--- db/migrate/20190824040322_add_document_analysis_indices.rb
+++ db/migrate/20190824040322_add_document_analysis_indices.rb
@@ -8,9 +8,5 @@ class AddDocumentAnalysisIndices < ActiveRecord::Migration[5.2]
 
     add_index :attributes, [:user_id, :deleted_at]
     add_index :attributes, [:attribute_field_id, :user_id, :entity_type, :entity_id, :deleted_at], name: :attributes_afi_ui_et_ei_da
-
-    add_index :motherships, [:mother_id, :character_id, :deleted_at]
-    add_index :fatherships, [:father_id, :character_id, :deleted_at]
-    #todo honestly we should probably do indices for all these triplet pairs (for each grouper)
   end
 end
