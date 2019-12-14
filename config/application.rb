a120f63eb572534e3ed579b6c01c0a315a9a8760
Andrew Brown <drusepth@gmail.com>
2019-12-14 01:32:20 -0600

    upgrade rails to 6.0.2

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,8 @@ Bundler.require(*Rails.groups)
 
 module Notebook
   class Application < Rails::Application
-    # Rails 6 should be autoloading everything under app/ now
-    # config.autoload_paths += Dir[Rails.root.join('app', 'models', '{*/}')]
-    # config.autoload_paths += Dir[Rails.root.join('app', 'services', '{*/}')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{*/}')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'services', '{*/}')]
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
