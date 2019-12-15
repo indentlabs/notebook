a05fe4f3f1e631860c7f0ebea0d3c3a0c47cdebf
Andrew Brown <drusepth@gmail.com>
2019-12-15 01:18:35 -0600

    add support for exporting notebooks in YML

--- config/routes.rb
+++ config/routes.rb
@@ -75,10 +75,11 @@ Rails.application.routes.draw do
       scope 'export' do
         get '/', to: 'export#index', as: :notebook_export
     
-        get '/outline', to: 'export#outline',             as: :notebook_outline
+        get '/outline',       to: 'export#outline',       as: :notebook_outline
         get '/notebook.json', to: 'export#notebook_json', as: :notebook_json
-        get '/notebook.xml', to: 'export#notebook_xml',   as: :notebook_xml
-        get '/:model.csv', to: 'export#csv',              as: :notebook_csv
+        get '/notebook.xml',  to: 'export#notebook_xml',  as: :notebook_xml
+        get '/notebook.yml',  to: 'export#notebook_yml',  as: :notebook_yml
+        get '/:model.csv',    to: 'export#csv',           as: :notebook_csv
       end
     end
 end
