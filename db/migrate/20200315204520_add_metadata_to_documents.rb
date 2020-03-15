957eac4a60e72cd909530d4c6d349f9a973c863e
Andrew Brown <drusepth@gmail.com>
2020-03-15 17:30:41 -0500

    start filling out metadata modal

--- db/migrate/20200315204520_add_metadata_to_documents.rb
+++ db/migrate/20200315204520_add_metadata_to_documents.rb
@@ -1,6 +1,8 @@
 class AddMetadataToDocuments < ActiveRecord::Migration[6.0]
   def change
+    add_column :documents, :subtitle, :string
     add_column :documents, :format, :string
+    add_column :documents, :genre, :string
     add_column :documents, :word_count, :integer
     add_column :documents, :status, :string
   end
