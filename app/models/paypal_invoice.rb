a6444ed8382571731f798fb33caf0a4c222ff7c9
Andrew Brown <drusepth@gmail.com>
2020-03-20 15:57:12 -0500

    tested to make sure it works ~

--- app/models/paypal_invoice.rb
+++ app/models/paypal_invoice.rb
@@ -31,6 +31,27 @@ class PaypalInvoice < ApplicationRecord
       description: "Prepaid Premium subscription"
     )
     self.save!
+
+    if PromoService.active?(:promo_bogo) && (self.months == 3 || self.months == 6)
+      # During this promo, we create a separate Invoice + PromoCode also
+      self.user.paypal_invoices.create(
+        paypal_id: "None -- Created from promo_bogo promo",
+        status:       self.status,
+        user_id:      self.user_id,
+        months:       self.months,
+        amount_cents: 0,
+        page_unlock_promo_code: PageUnlockPromoCode.create(
+          code:                 'BOGO-' + (0...8).map { (65 + rand(26)).chr }.join + '-' + (0...8).map { (65 + rand(26)).chr }.join,
+          page_types:           Rails.application.config.content_types[:premium].map(&:name),
+          uses_remaining:       1,
+          days_active:          30 * self.months.to_i,
+          internal_description: "promo_bogo duplicate code",
+          description:          "Your free buy-one-get-one-free Premium Code",
+        ),
+        approval_url: nil,
+        payer_id:     self.payer_id
+      )
+    end
   end
 
   def activateable?
