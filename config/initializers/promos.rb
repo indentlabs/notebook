e020ae1a2d6e1fc9e10214bc7679087cc6c8cd94
Andrew Brown <drusepth@gmail.com>
2020-03-25 14:01:29 -0500

    lore fields

--- config/initializers/promos.rb
+++ config/initializers/promos.rb
@@ -1,5 +1,13 @@
 Rails.application.config.promos = {}
 
+# BOGO on Premium Codes
 Rails.application.config.promos[:promo_bogo] = {}
 Rails.application.config.promos[:promo_bogo][:start_date] = 'March 21, 2020'.to_date
 Rails.application.config.promos[:promo_bogo][:end_date] = Rails.application.config.promos[:promo_bogo][:start_date] + 2.weeks
+
+# Lore free during the month of April
+if Date.current >= 'March 1, 2020'.to_date
+  if Date.current < 'May 1, 2020'.to_date
+    Rails.application.config.content_types[:free] << Lore
+  end
+end
