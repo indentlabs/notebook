9e0249f7a9b83f70d43ddc51b326ea24389331a4
Andrew Brown <drusepth@gmail.com>
2020-03-26 18:24:04 -0500

    two-way link fixes

--- app/models/page_groupers/group_enemyship.rb
+++ app/models/page_groupers/group_enemyship.rb
@@ -15,6 +15,6 @@ class GroupEnemyship < ApplicationRecord
     this_object  = Group.find_by(id: self.group_id)
     other_object = Group.find_by(id: self.enemy_id)
 
-    other_object.enemies.delete this_object
+    other_object.enemies.delete(this_object) if other_object.present?
   end
 end
