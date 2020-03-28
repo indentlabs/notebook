38dda2e079127a0a6ecd6fdfe1ede5f0614920f8
Andrew Brown <drusepth@gmail.com>
2020-03-28 18:36:06 -0500

    don't let blocked users follow their blockers

--- app/models/user_blocking.rb
+++ app/models/user_blocking.rb
@@ -1,4 +1,10 @@
 class UserBlocking < ApplicationRecord
   belongs_to :user
   belongs_to :blocked_user, class_name: User.name
+
+  after_create do
+    # If a user blocks someone, we should automatically unfollow both pairings
+    UserFollowing.find_by(user_id: self.user_id, followed_user_id: self.blocked_user_id).try(:destroy)
+    UserFollowing.find_by(user_id: self.blocked_user_id, followed_user_id: self.user_id).try(:destroy)
+  end
 end
