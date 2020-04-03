4a662f4d773a63fea4561698155db355666686a3
Andrew Brown <drusepth@gmail.com>
2020-04-03 13:04:58 -0500

    Add some safety checking around image_processing avatar processing https://github.com/janko/image_processing/issues/66#issuecomment-608573201

--- app/models/users/user.rb
+++ app/models/users/user.rb
@@ -152,12 +152,18 @@ class User < ApplicationRecord
       Rails.application.routes.url_helpers.rails_representation_url(avatar.variant(resize_to_limit: [size, size]).processed, only_path: true)
 
     else # otherwise, grab the default from Gravatar for this email address
-      require 'digest/md5' # todo do we actually need to require this all the time?
-      email_md5 = Digest::MD5.hexdigest(email.downcase)
-      "https://www.gravatar.com/avatar/#{email_md5}?d=identicon&s=#{size}".html_safe
+      gravatar_fallback_url
     end
 
   rescue ActiveStorage::FileNotFoundError
+    gravatar_fallback_url
+
+  rescue ImageProcessing::Error
+    gravatar_fallback_url
+  end
+
+  def gravatar_fallback_url
+    require 'digest/md5' # todo do we actually need to require this all the time?
     email_md5 = Digest::MD5.hexdigest(email.downcase)
     "https://www.gravatar.com/avatar/#{email_md5}?d=identicon&s=#{size}".html_safe
   end
