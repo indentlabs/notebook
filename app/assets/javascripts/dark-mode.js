5e90d8fedd7766a9eb88cb74445cbe0efc562dde
Andrew Brown <drusepth@gmail.com>
2020-05-15 18:17:57 -0500

    give the people what they want

---
+++ app/assets/javascripts/dark-mode.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.dark-toggle').on('click',function(){
+    if ($(this).find('i').text() == 'brightness_4'){
+        $(this).find('i').text('brightness_high');
+    } else {
+        $(this).find('i').text('brightness_4');
+    }
+  });
+});
