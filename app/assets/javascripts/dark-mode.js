e974b665487440f4316c5a851c22354f47937326
Andrew Brown <drusepth@gmail.com>
2020-06-01 15:16:55 -0500

    polish off dark mode v1

--- app/assets/javascripts/dark-mode.js
+++ app/assets/javascripts/dark-mode.js
@@ -1,9 +1,34 @@
 $(document).ready(function(){
-  $('.dark-toggle').on('click',function(){
-    if ($(this).find('i').text() == 'brightness_4'){
-        $(this).find('i').text('brightness_high');
+  $('.dark-toggle > *').on('click', function() {
+    alert('togglin');
+    var toggle_icon = $(this).find('i');
+    var light_mode_icon = 'brightness_high',
+        dark_mode_icon  = 'brightness_4';
+    var dark_mode_enabled = $('body').hasClass('dark');
+
+    window.localStorage.setItem('dark_mode_enabled', !dark_mode_enabled);
+    toggle_icon.text(dark_mode_enabled ? light_mode_icon : dark_mode_icon);
+
+    if (dark_mode_enabled) {
+      $('body').removeClass('dark');
     } else {
-        $(this).find('i').text('brightness_4');
+      $('body').addClass('dark');
     }
+
+    // Update dark mode preferences server-side so we can enable it at page load 
+    // and avoid any light-to-dark blinding flashes
+    $.ajax({
+      type:     "PUT",
+      dataType: "json",
+      url:      '/users',
+      data: {
+        'user': {
+          'dark_mode_enabled': !dark_mode_enabled
+        }
+      }
+    });
+
+    console.log('dark mode is now ' + !dark_mode_enabled);
+
   });
 });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_021338) do
+ActiveRecord::Schema.define(version: 2020_06_01_122635) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -3271,6 +3271,7 @@ ActiveRecord::Schema.define(version: 2020_05_02_021338) do
     t.string "favorite_quote"
     t.string "occupation"
     t.string "favorite_page_type"
+    t.boolean "dark_mode_enabled"
     t.index ["deleted_at", "username"], name: "index_users_on_deleted_at_and_username"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["id", "deleted_at"], name: "index_users_on_id_and_deleted_at"
