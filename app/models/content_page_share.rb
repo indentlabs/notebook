bfec7797ab76ec390ea5641afe15990fdcbbf67f
Andrew Brown <drusepth@gmail.com>
2020-07-06 17:12:03 -0700

    allow sharing documents

--- app/models/content_page_share.rb
+++ app/models/content_page_share.rb
@@ -1,7 +1,7 @@
 class ContentPageShare < ApplicationRecord
   belongs_to :user
-  belongs_to :content_page,           polymorphic: true
-  belongs_to :secondary_content_page, polymorphic: true
+  belongs_to :content_page,           polymorphic: true, optional: true
+  belongs_to :secondary_content_page, polymorphic: true, optional: true
 
 
   has_many :share_comments,                dependent: :destroy
