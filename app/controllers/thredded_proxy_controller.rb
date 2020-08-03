992b3fd3c2e4448b37211b57694f4e106feab617
Andrew Brown <drusepth@gmail.com>
2020-08-02 22:39:48 -0700

    a couple minor 500s

--- app/controllers/thredded_proxy_controller.rb
+++ app/controllers/thredded_proxy_controller.rb
@@ -1,9 +1,13 @@
 class ThreddedProxyController < ApplicationController
   def topic
     topic = Thredded::Topic.find_by(slug: params[:slug])
-    redirect_to thredded.messageboard_topic_path(
-      messageboard_id: topic.messageboard_id,
-      id:              topic.id
-    )
+    if topic.present?
+      redirect_to thredded.messageboard_topic_path(
+        messageboard_id: topic.messageboard_id,
+        id:              topic.id
+      )
+    else
+      redirect_back(fallback_location: root_path, notice: "The link you tried to visit is invalid or no longer exists.")
+    end
   end
 end
