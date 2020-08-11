f9e7db7e472976940e684ab04d61936339e38399
Andrew Brown <drusepth@gmail.com>
2020-08-10 19:05:43 -0700

    disambiguating permissions service referneces

--- app/authorizers/content_authorizer.rb
+++ app/authorizers/content_authorizer.rb
@@ -1,11 +1,11 @@
 class ContentAuthorizer < ApplicationAuthorizer
   def readable_by? user
     [
-      PermissionService.user_owns_any_containing_universe?(user: user, content: resource),
-      PermissionService.user_owns_content?(user: user, content: resource),
-      PermissionService.content_is_public?(content: resource),
-      PermissionService.content_is_in_a_public_universe?(content: resource),
-      PermissionService.user_can_contribute_to_containing_universe?(user: user, content: resource)
+      ::PermissionService.user_owns_any_containing_universe?(user: user, content: resource),
+      ::PermissionService.user_owns_content?(user: user, content: resource),
+      ::PermissionService.content_is_public?(content: resource),
+      ::PermissionService.content_is_in_a_public_universe?(content: resource),
+      ::PermissionService.user_can_contribute_to_containing_universe?(user: user, content: resource)
     ].any?
   end
 
