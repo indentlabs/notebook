3f2f3a7b3cc8504aa4ee1edcf1e8ffd2828ac00c
Andrew Brown <drusepth@gmail.com>
2021-01-25 01:44:07 -0800

    disable thredded emails again

--- config/initializers/thredded.rb
+++ config/initializers/thredded.rb
@@ -151,10 +151,10 @@ Thredded.layout = 'layouts/forum'
 # Change how users can choose to be notified, by adding notifiers here, or removing the initializer altogether
 #
 # default:
-Thredded.notifiers = [Thredded::EmailNotifier.new]
+# Thredded.notifiers = [Thredded::EmailNotifier.new]
 #
 # none:
-# Thredded.notifiers = []
+Thredded.notifiers = []
 #
 # add in (must install separate gem (under development) as well):
 # Thredded.notifiers = [Thredded::EmailNotifier.new, Thredded::PushoverNotifier.new(ENV['PUSHOVER_APP_ID'])]
