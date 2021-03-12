3bcf39d554ae513c7c0be599413197644ca0bf52
Andrew Brown <drusepth@gmail.com>
2021-03-11 22:42:27 -0800

    scale back indices

--- db/migrate/20210308203925_add_more_indexes_march82021.rb
+++
@@ -1,5 +0,0 @@
-class AddMoreIndexesMarch82021 < ActiveRecord::Migration[6.0]
-  def change
-    add_index :contributors, [:email, :user_id]
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_064749) do
+ActiveRecord::Schema.define(version: 2021_03_08_200757) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -3136,6 +3136,11 @@ ActiveRecord::Schema.define(version: 2021_03_08_064749) do
     t.integer "moderation_state", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at", "messageboard_id"], name: "index_thredded_posts_on_deleted_at_and_messageboard_id"
+    t.index ["deleted_at", "postable_id"], name: "index_thredded_posts_on_deleted_at_and_postable_id"
+    t.index ["deleted_at", "user_id"], name: "index_thredded_posts_on_deleted_at_and_user_id"
+    t.index ["deleted_at"], name: "index_thredded_posts_on_deleted_at"
     t.index ["messageboard_id"], name: "index_thredded_posts_on_messageboard_id"
     t.index ["moderation_state", "updated_at"], name: "index_thredded_posts_for_display"
     t.index ["postable_id"], name: "index_thredded_posts_on_postable_id"
@@ -3197,6 +3202,10 @@ ActiveRecord::Schema.define(version: 2021_03_08_064749) do
     t.datetime "last_post_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at", "messageboard_id"], name: "index_thredded_topics_on_deleted_at_and_messageboard_id"
+    t.index ["deleted_at", "user_id"], name: "index_thredded_topics_on_deleted_at_and_user_id"
+    t.index ["deleted_at"], name: "index_thredded_topics_on_deleted_at"
     t.index ["hash_id"], name: "index_thredded_topics_on_hash_id"
     t.index ["last_post_at"], name: "index_thredded_topics_on_last_post_at"
     t.index ["messageboard_id"], name: "index_thredded_topics_on_messageboard_id"
@@ -3539,6 +3548,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_064749) do
     t.boolean "notification_updates", default: true
     t.boolean "community_features_enabled", default: true
     t.boolean "private_profile", default: false
+    t.boolean "enabled_april_fools"
     t.index ["deleted_at", "username"], name: "index_users_on_deleted_at_and_username"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["id", "deleted_at"], name: "index_users_on_id_and_deleted_at"
