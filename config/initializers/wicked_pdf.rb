8f947ae9bfdb90e2d37f4fa466c9112dd0e03f42
Andrew Brown <drusepth@gmail.com>
2021-03-29 16:39:53 -0700

    try this path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -14,7 +14,7 @@ WickedPdf.config = {
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-  :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s,
+  :exe_path => Rails.root.join('vendor/bundle/bin/wkhtmltopdf-linux-amd64').to_s,
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
