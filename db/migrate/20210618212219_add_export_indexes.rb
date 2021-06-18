ab5f6a8cdc0cad397ff96692bb81cf522fdda57b
Andrew Brown <drusepth@gmail.com>
2021-06-18 16:14:34 -0700

    Revert "add indexes for exports"
    This reverts commit 99c761881eea45ff5311deb91b3eab65c7c9c7ce.

--- db/migrate/20210618212219_add_export_indexes.rb
+++
@@ -1,6 +0,0 @@
-class AddExportIndexes < ActiveRecord::Migration[6.0]
-  def change
-    add_index :attributes, [:deleted_at, :user_id, :attribute_field_id, :entity_type, :entity_id], name: "all_the_export_fields"
-    add_index :attributes, [:deleted_at, :user_id, :attribute_field_id, :entity_type, :entity_id, :id], name: "all_the_export_fields_with_sort"
-  end
-end
