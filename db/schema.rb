3a87081c7c77464fd66d2c5bac7efca4fb1b6084
Andrew Brown <drusepth@gmail.com>
2021-06-20 14:58:31 -0700

    add migration for word count cache on docs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_212219) do
+ActiveRecord::Schema.define(version: 2021_06_20_214754) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
