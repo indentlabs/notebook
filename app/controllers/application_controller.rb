258be6f9179397a33abc33ec4e23300efa73deb3
Andrew Brown <drusepth@gmail.com>
2021-08-03 01:00:11 -0700

    -1 query

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
         session.delete(:universe_id)
       elsif params[:universe].is_a?(String) && params[:universe].to_i.to_s == params[:universe]
         found_universe = Universe.find_by(id: params[:universe])
-        found_universe = nil unless current_user.universes.include?(found_universe) || current_user.contributable_universes.include?(found_universe)
+        found_universe = nil unless found_universe.user_id == current_user.id || current_user.contributable_universes.include?(found_universe)
         session[:universe_id] = found_universe.id if found_universe
       end
     end
