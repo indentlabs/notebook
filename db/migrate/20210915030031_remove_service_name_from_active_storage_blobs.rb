7ea89ecac63fcf94dacf90d58a38a4ee9659df46
Andrew Brown <drusepth@gmail.com>
2021-09-14 20:12:12 -0700

    actually, lets just tackle it this way

--- db/migrate/20210915030031_remove_service_name_from_active_storage_blobs.rb
+++ db/migrate/20210915030031_remove_service_name_from_active_storage_blobs.rb
@@ -1,19 +1,13 @@
 class RemoveServiceNameFromActiveStorageBlobs < ActiveRecord::Migration[6.0]
   def up
     if column_exists?(:active_storage_blobs, :service_name)
-      remove_column :active_storage_blobs, :service_name
+      change_column_default :active_storage_blobs, :service_name, "amazon"
     end
   end
 
   def down
-    unless column_exists?(:active_storage_blobs, :service_name)
-      add_column :active_storage_blobs, :service_name, :string
-
-      if configured_service = ActiveStorage::Blob.service.name
-        ActiveStorage::Blob.unscoped.update_all(service_name: configured_service)
-      end
-
-      change_column :active_storage_blobs, :service_name, :string, null: false
+    if column_exists?(:active_storage_blobs, :service_name)
+      change_column_default :active_storage_blobs, :service_name, nil
     end
   end
 end
