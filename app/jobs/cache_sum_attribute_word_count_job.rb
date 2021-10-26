af9b47839ef26a6ebc4eb3386017c19a9759ce71
drusepth <drusepth@gmail.com>
2021-10-25 21:06:07 -0700

    safety on job

--- app/jobs/cache_sum_attribute_word_count_job.rb
+++ app/jobs/cache_sum_attribute_word_count_job.rb
@@ -6,8 +6,9 @@ class CacheSumAttributeWordCountJob < ApplicationJob
     entity_id   = args.shift
 
     entity = entity_type.constantize.find_by(id: entity_id)
-    sum_attribute_word_count = Attribute.where(entity_type: entity_type, entity_id: entity_id).sum(:word_count_cache)
+    return if entity.nil?
 
+    sum_attribute_word_count = Attribute.where(entity_type: entity_type, entity_id: entity_id).sum(:word_count_cache)
     update = entity.word_count_updates.find_or_initialize_by(
       for_date: DateTime.current,
     )
