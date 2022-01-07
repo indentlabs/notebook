a73a5333f95b0a1c68d81d9295194cfcbb3f7c10
drusepth <drusepth@gmail.com>
2022-01-06 19:14:29 -0800

    add tailwind and start on landing page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,8 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import "../application.css";
+
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
