6f52e0961ac6120c0917fdd13cbb7cdae929808a
drusepth <drusepth@gmail.com>
2022-02-05 19:52:16 -0800

    style new sign in form

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
   post 'customization/toggle_content_type'
 
   # User-centric stuff
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations', sessions: 'sessions' }
   resources :users do
     devise_scope :user do
       get 'preferences',  to: 'registrations#preferences'
