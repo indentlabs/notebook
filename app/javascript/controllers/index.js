719ce145e6203757dbee56e6cb97fa39ec5463fb
drusepth <drusepth@gmail.com>
2022-04-15 14:09:05 -0700

    get first stimulusjs dropdown working

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,9 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import Dropdown from 'stimulus-dropdown'
 
 const application = Application.start()
+application.register('dropdown', Dropdown)
+
 const context = require.context(".", true, /\.js$/)
 application.load(definitionsFromContext(context))
