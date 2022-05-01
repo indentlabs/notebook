7dff1b3daf2c1424146a83f9444c90c86bead6ee
drusepth <drusepth@gmail.com>
2022-04-30 20:09:01 -0700

    wip

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,12 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+
 import Dropdown from 'stimulus-dropdown'
+import AnimatedNumber from 'stimulus-animated-number'
 
 const application = Application.start()
 application.register('dropdown', Dropdown)
+application.register('animated-number', AnimatedNumber)
 
 const context = require.context(".", true, /\.js$/)
 application.load(definitionsFromContext(context))
