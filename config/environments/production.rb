c100179078af803c90bcf9ea2fd2f38f5f0f3b53
Andrew Brown <drusepth@gmail.com>
2023-02-14 12:49:00 -0800

    whoopsie daisies typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = Uglifier.new(harmony: true)
-  config.assets.js_compressor = :terser (alternative for ES6)
+  config.assets.js_compressor = :terser # (alternative for ES6)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
