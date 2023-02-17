291e5c40f7a71433aecc26a43c60661f50583947
Andrew Brown <drusepth@gmail.com>
2023-02-17 11:49:40 -0800

    bring the errors

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,11 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
-// import Dropdown from 'stimulus-dropdown'
+import Dropdown from 'stimulus-dropdown'
 import AnimatedNumber from 'stimulus-animated-number'
 
 const application = Application.start()
-// application.register('dropdown', Dropdown)
+application.register('dropdown', Dropdown)
 application.register('animated-number', AnimatedNumber)
 
 const context = require.context(".", true, /\.js$/)
