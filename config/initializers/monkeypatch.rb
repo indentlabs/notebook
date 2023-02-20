5d3e9309646cb292e5af937aedfbc7faba03c09f
Andrew Brown <drusepth@gmail.com>
2023-02-20 14:05:07 -0800

    ok so the monkeypatch is needed after all <3

---
+++ config/initializers/monkeypatch.rb
@@ -0,0 +1,10 @@
+# We need to monkeypatch this in because paperclip internally uses URI.escape, and
+# Rails 3 drops URI.escape support in favor of CGI.escape. After we've migrated off
+# paperclip (or paperclip stops sucking), we can remove this monkeypatch.
+
+module URI
+  def URI.escape(url)
+    #CGI.escape(url)
+    url
+  end
+end
