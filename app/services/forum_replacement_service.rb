e7c5810b107a3cddd329dcafdfe2159863956aae
Andrew Brown <drusepth@gmail.com>
2023-04-07 16:33:29 -0700

    lock the gremlins up

--- app/services/forum_replacement_service.rb
+++ app/services/forum_replacement_service.rb
@@ -472,7 +472,7 @@ class ForumReplacementService < Service
   }
 
   def self.replace_for(text, user)
-    gremlins_phase = 1
+    gremlins_phase = 0
     replaced_text = text.dup
 
     # Page tag replacements
