1577eccc7bf890616667a25b4f36e047c60f3576
Andrew Brown <drusepth@gmail.com>
2023-04-18 19:55:08 -0700

    auto-expand worldbuilding sidebar pages

--- app/controllers/customization_controller.rb
+++ app/controllers/customization_controller.rb
@@ -1,16 +1,14 @@
 class CustomizationController < ApplicationController
   layout 'tailwind'
 
-  # todo require login for all actions :O
-
+  before_action :authenticate_user!
   before_action :verify_content_type_can_be_toggled, only: [:toggle_content_type]
 
   def content_types
-    return redirect_to(root_path) unless user_signed_in?
-
     @all_content_types = Rails.application.config.content_types[:all]
     @premium_content_types = Rails.application.config.content_types[:premium]
     @my_activators = current_user.user_content_type_activators.pluck(:content_type)
+
     @sidenav_expansion = 'worldbuilding'
     @page_title = "Customize your notebook pages"
   end
