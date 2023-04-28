718744bacdef50ab407b180f36e838698a0382e7
Andrew Brown <drusepth@gmail.com>
2023-04-28 13:57:25 -0700

    more search polish, filters, sorts

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,12 +4,28 @@ class SearchController < ApplicationController
   layout 'tailwind'
 
   def results
-    @query = params[:q]
+    @query  = params[:q]
+    @sort   = params[:sort] || 'relevance'
+    @filter = params[:filter] || 'all'
 
     @matched_attributes = Attribute
       .where(user_id: current_user.id)
       .where('value ILIKE ?', "%#{@query}%")
-      .order(:entity_type, :entity_id)
+
+    @result_types = @matched_attributes.pluck(:entity_type).uniq
+
+    if @filter != 'all'
+      @matched_attributes = @matched_attributes.where(entity_type: @filter)
+    end
+
+    case @sort
+    when 'relevance'
+      @matched_attributes = @matched_attributes.order(:entity_type, :entity_id)
+    when 'recent'
+      @matched_attributes = @matched_attributes.order(created_at: :desc)
+    when 'oldest'
+      @matched_attributes = @matched_attributes.order(created_at: :asc)
+    end
 
     @seen_result_pages = {}
   end
