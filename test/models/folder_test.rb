0c871620a54a505e872c7148e47ffd7af9f67617
Andrew Brown <drusepth@gmail.com>
2025-04-23 23:14:35 -0700

    improve Folder tests

--- test/models/folder_test.rb
+++ test/models/folder_test.rb
@@ -3,22 +3,38 @@ require 'test_helper'
 class FolderTest < ActiveSupport::TestCase
   def setup
     @user = user(:starter)
+    @other_user = user(:premium)
     @parent_folder = Folder.create!(title: 'Parent Folder', user: @user)
     @folder = Folder.create!(title: 'Test Folder', user: @user, parent_folder: @parent_folder)
   end
 
-  test "should be valid" do
+  # Basic validation tests
+  test "should be valid with required attributes" do
     assert @folder.valid?
   end
 
   test "should require a user" do
     @folder.user = nil
     assert_not @folder.valid?
+    assert_includes @folder.errors[:user], "must exist"
   end
 
+  test "should be valid without a parent folder" do
+    top_level_folder = Folder.new(title: 'Top Level', user: @user)
+    assert top_level_folder.valid?
+  end
+
+  # Association tests
   test "should have many documents" do
     assert_respond_to @folder, :documents
     assert_kind_of ActiveRecord::Associations::CollectionProxy, @folder.documents
+  end
+
+  test "documents association should work" do
+    document = Document.create!(title: 'Test Document', user: @user)
+    @folder.documents << document
+    assert_includes @folder.documents, document
+    assert_equal @folder, document.folder
   end
 
   test "should belong to parent folder" do
@@ -31,19 +47,34 @@ class FolderTest < ActiveSupport::TestCase
     assert_equal @user, @folder.user
   end
 
+  # Child folders tests with edge cases
   test "child_folders should return folders with same user and parent" do
     child_folder = Folder.create!(title: 'Child Folder', user: @user, parent_folder: @folder)
     assert_includes @folder.child_folders, child_folder
   end
 
-  test "class methods should return correct values" do
-    assert_equal 'lighten-1 teal', Folder.color
-    assert_equal 'text-lighten-1 teal-text', Folder.text_color
-    assert_equal 'folder', Folder.icon
+  test "child_folders should not return folders from different users" do
+    other_user_folder = Folder.create!(title: 'Other User Folder', user: @other_user, parent_folder: @folder)
+    assert_not_includes @folder.child_folders, other_user_folder
   end
 
+  test "child_folders should not return folders with different parents" do
+    other_parent = Folder.create!(title: 'Other Parent', user: @user)
+    different_parent_folder = Folder.create!(title: 'Different Parent', user: @user, parent_folder: other_parent)
+    assert_not_includes @folder.child_folders, different_parent_folder
+  end
+
+  # to_param tests with semantic checks
   test "to_param should include id and slugged title" do
-    expected_param = "#{@folder.id}-#{PageTagService.slug_for(@folder.title)}"
-    assert_equal expected_param, @folder.to_param
+    param = @folder.to_param
+    assert_match(/^\d+-[\w-]+$/, param)
+    assert_includes param, @folder.id.to_s
+  end
+
+  test "to_param should handle special characters in title" do
+    folder = Folder.create!(title: 'Test & Folder!', user: @user)
+    param = folder.to_param
+    assert_match(/^\d+-[\w-]+$/, param)
+    assert_includes param, folder.id.to_s
   end
 end
