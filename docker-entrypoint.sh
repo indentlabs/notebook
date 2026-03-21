be3089c6db3fe7e9e81550f6e1df186015e0ef1d
Andrew Brown <drusepth@gmail.com>
2026-03-21 00:13:58 -0700

    Explicitly run db:seed on container boot to guarantee test user creation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,7 @@ rm -f /home/notebookai/tmp/pids/server.pid
 
 # Ensure the database is prepared (migrated & seeded) before starting the server
 bundle exec rails db:prepare
+bundle exec rails db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
