c8c7d108eb9770ede223f2140c108cc95ed1a76c
Andrew Brown <drusepth@gmail.com>
2026-03-20 22:20:24 -0700

    Allow Railway hostnames in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Allow PR environments on Railway (auto-deployed dynamic domains)
+  config.hosts << /.*\.up\.railway\.app/
+  config.hosts << ENV["RAILWAY_PUBLIC_DOMAIN"] if ENV["RAILWAY_PUBLIC_DOMAIN"].present?
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
