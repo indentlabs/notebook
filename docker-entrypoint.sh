c7ef08afcb7f5864bcc5dbc788a22fc32ea1c200
Andrew Brown <drusepth@gmail.com>
2026-03-20 22:38:11 -0700

    Move db:prepare to runtime entrypoint so SQLite persists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /home/notebookai/tmp/pids/server.pid
 
+# Ensure the database is prepared (migrated & seeded) before starting the server
+bundle exec rails db:prepare
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
